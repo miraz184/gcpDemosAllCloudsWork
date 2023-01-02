9d2a939cd50ce0868fe95fec3d2c066ecb1ecffe
mirazul <sk.haque@globallogic.com>
2023-01-02 16:59:27 +0530

    app engine added

---
+++ appengine/bucketdemo/src/main/java/com/example/bootdemo/BucketController.java
@@ -0,0 +1,42 @@
+package com.example.bootdemo;
+
+
+import com.google.cloud.ReadChannel;
+import com.google.cloud.storage.Storage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+
+@RequestMapping("/files")
+@RestController
+public class BucketController {
+
+    @Autowired
+    private Storage storage;
+
+    @GetMapping("/trainees")
+    public String fetchTrainees() throws Exception {
+        StringBuilder builder = new StringBuilder();
+        try (ReadChannel channel = storage.reader("vinproject7-securedbucket", "names.txt")) {
+            // converted from nio to io for understanding, nio should be directly used for performance
+            InputStream input = Channels.newInputStream(channel);
+            BufferedInputStream bin = new BufferedInputStream(input);
+            byte[] buffer = new byte[64 * 1000];
+            int readCount;
+            while ((readCount = bin.read(buffer)) > 0) {
+                String readString = new String(buffer,0,readCount);
+                builder.append(readString);
+            }
+        }
+        return builder.toString();
+    }
+
+
+}
